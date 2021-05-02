2c51c5f63f3cddd1548999bb9e66a1a2bd01de12
muhmadjaradat <muhmadjradat@gmail.com>
2021-05-03 00:01:20 +0300

    adding lab06

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react'
+import axios from 'axios';
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import City from './component/City'
+import Error from './component/Alert'
+
+export class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      query:'',
+      data: '',
+      show:false,
+      status:''
+    }
+  }
+  getLocation = async (e)=> {
+    e.preventDefault();
+    const url=`https://eu1.locationiq.com/v1/search.php?key=pk.2f919ae7e90e6f305a9071c9f154f32a&q=${this.state.query}&format=json`;
+    const reqData= await axios.get(url);
+    console.log(reqData);
+    this.setState({
+      data:reqData.data[0],
+      show:true,
+      status:reqData.status
+    })
+  }
+  updateData = (e)=>{
+    this.setState({
+      query:e.target.value
+    })
+  }
+  render() {
+    return (
+      <div>
+        <h1>City Explorer</h1>
+        <Form>
+  <Form.Group controlId="formBasicEmail" >
+    
+    <Form.Control onChange={this.updateData} type="text" placeholder="Enter a city name" name='name' />
+  </Form.Group>
+
+  <Button variant="primary" type="submit" onClick={this.getLocation}>
+    Explore!
+  </Button>
+</Form>
+{(Number(this.state.status)===200) ? <City query={this.state.query} data={this.state.data} show={this.state.show} />:null }
+
+
+      </div>
+  
+    )
+  }
 }
 
-export default App;
+export default App
+
+
+
+
---
+++ src/component/City.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container'
+import {Row,Col,Card,ListGroup,Image}from 'react-bootstrap'
+
+
+export class City extends React.Component {
+    render() {
+        return (
+            <div>
+                <Container >
+  <Row>
+    <Col>
+    <Card style={{ width: '18rem' }}>
+  <Card.Header>{this.props.query}</Card.Header>
+  <ListGroup variant="flush">
+    <ListGroup.Item>Latitude:{this.props.data.lat}</ListGroup.Item>
+    <ListGroup.Item>Longitude:{this.props.data.lon}</ListGroup.Item>
+  </ListGroup>
+</Card>
+    </Col>
+    <Col><Image src={`https://maps.locationiq.com/v3/staticmap?key=pk.d36871f015649f915282f374cff76628&q&center=${this.props.data.lat},${this.props.data.lon}&zoom=10&markers=size:small|color:red|${this.props.data.lat},${this.props.data.lon}`} fluid /></Col>
+  </Row>
+</Container>
+            </div>
+        )
+    }
+}
+
+export default City
